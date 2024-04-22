19b1780053003ca773bddd2e2231453c65f7959d
bastian <bastian@078-051.eduroam.rwth-aachen.de>
2024-04-22 15:05:45 +0200

    gogogo

---
+++ Versuch1/Variablen.cpp
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 
+// 
+// Versuch 1.1: Datentypen und Typumwandlung
+//
+// Datei:  Variablen.cpp
+// Inhalt: Hauptprogramm
+//////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int iErste = 0;
+    int iZweite = 0;
+    
+    // Hier folgt Ihr eigener Code
+    
+    return 0;
+    
+}
