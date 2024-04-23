c285e1c183f83c821ad102269b69585b2177d49d
bastian <bastian@078-051.eduroam.rwth-aachen.de>
2024-04-23 11:40:35 +0200

    Noch nie habe ich mich so useless gefühlt beim schreiben von Code =)

--- Versuch1Teil2/Strukturen.cpp
+++ Versuch1Teil2/Strukturen.cpp
@@ -9,8 +9,65 @@
 #include <iostream>
 #include <string>
 
+
+using namespace std;
+
+
+struct Person {
+	string sNachname;
+	string sVorname;
+	int iGeburtsjahr;
+	int iAlter;
+};
+
+
+void vPersonAusgeben(Person pPerson) {
+	cout <<
+				"Du bist also " << pPerson.sVorname << " " << pPerson.sNachname <<
+				", wurdest " << pPerson.iGeburtsjahr <<
+				" geboren und bist " << pPerson.iAlter <<
+				" Jahre alt! Schön dich kennenzulernen!" <<
+				endl;
+}
+
+
+
 int main()
 {
-    return 0;
+
+	Person nBenutzer;
+
+
+	cout << "Bitte gib nun dein Vornamen ein: ";
+	cin >> nBenutzer.sVorname;
+	cout << "Bitte gib nun dein Nachnamen ein: ";
+	cin >> nBenutzer.sNachname;
+	cout << "Bitte gib nun dein Geburtsjahr ein: ";
+	cin >> nBenutzer.iGeburtsjahr;
+	cout << "Bitte gib nun dein Alter ein: ";
+	cin >> nBenutzer.iAlter;
+
+
+	cout << "Super!" << endl;
+
+	vPersonAusgeben(nBenutzer);
+
+	Person nKopieGesamt = nBenutzer;
+	Person nKopieEinzeln;
+
+	nKopieEinzeln.sVorname = nBenutzer.sVorname;
+	nKopieEinzeln.sNachname = nBenutzer.sNachname;
+	nKopieEinzeln.iGeburtsjahr = nBenutzer.iGeburtsjahr;
+	nKopieEinzeln.iAlter = nBenutzer.iAlter;
+
+	cout << "Nun kommt gesamtkopie:" << endl;
+	vPersonAusgeben(nKopieGesamt);
+
+	cout << "Nun kommt einzelkopie:" << endl;
+	vPersonAusgeben(nKopieEinzeln);
+
+	// Noch nie habe ich mich mehr useless gefühlt.
+
+    return 1;
     
 }
