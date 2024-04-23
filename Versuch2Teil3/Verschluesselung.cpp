7fe13b75cfeb499b40d09eae3b829e814d331877
bastian <bastian@083-118.eduroam.rwth-aachen.de>
2024-04-23 13:01:47 +0200

    Import von Versuch2Teil3

---
+++ Versuch2Teil3/Verschluesselung.cpp
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+// Praktikum Informatik 1 
+// 
+// Versuch 2.3: Felder
+//
+// Datei:  Verschuesselung.cpp
+// Inhalt: Hauptprogramm
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+
+string verschluesseln(char schluessel[2][26], string wort)
+{
+	// Hier folgt Ihr Code
+
+	return wort;
+}
+
+string entschluesseln(char schluessel[2][26], string wort)
+{
+	// Hier folgt Ihr Code
+
+	return wort;
+}
+
+int main()
+{
+	// Hier folgt Ihr Code
+
+	return 0;
+}
